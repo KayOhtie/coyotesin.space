cc44c49562e9bc0ac176d34dec5a8d6b072cae51
Kay <kay@coyotesin.space>
2018-10-21 00:17:15 -0500

    updating pullscript, readme for cloning

--- pull_bulma.sh
+++ pull_bulma.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# change next tag to current version
+version="0.7.2"
+# don't change
 mkdir -p .git/modules/theme/source/scss/bulma/info 2>/dev/null
 echo "css/*" > .git/modules/theme/source/scss/bulma/info/sparse-checkout
 echo "sass/*" >> .git/modules/theme/source/scss/bulma/info/sparse-checkout
@@ -9,8 +12,5 @@ echo "package.json" >> .git/modules/theme/source/scss/bulma/info/sparse-checkout
 echo "README.md" >> .git/modules/theme/source/scss/bulma/info/sparse-checkout
 cd theme/source/scss/bulma
 git config core.sparseCheckout true
-# change next tag to current version
-version="0.7.2"
-git fetch origin $version
-git pull origin $version
+git fetch
 git checkout $version
