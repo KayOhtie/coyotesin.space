6a19c080b2effd0c6454dcf6780b74047c0c6522
Kay <kay@coyotesin.space>
2018-10-20 23:56:49 -0500

    fixed script again

--- pull_bulma.sh
+++ pull_bulma.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-git config core.sparseCheckout true
-md -p .git/modules/theme/source/scss/bulma/info
+mkdir -p .git/modules/theme/source/scss/bulma/info 2>/dev/null
 echo "css/*" > .git/modules/theme/source/scss/bulma/info/sparse-checkout
 echo "sass/*" >> .git/modules/theme/source/scss/bulma/info/sparse-checkout
 echo "bulma.sass" >> .git/modules/theme/source/scss/bulma/info/sparse-checkout
@@ -9,5 +8,6 @@ echo "LICENSE" >> .git/modules/theme/source/scss/bulma/info/sparse-checkout
 echo "package.json" >> .git/modules/theme/source/scss/bulma/info/sparse-checkout
 echo "README.md" >> .git/modules/theme/source/scss/bulma/info/sparse-checkout
 cd theme/source/scss/bulma
+git config core.sparseCheckout true
 # change next tag to current version
 git checkout 0.7.2
