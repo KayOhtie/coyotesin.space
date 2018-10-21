6ecf7c0409ddc04236b2c19780bfd8e9eed89404
Kay <kay@coyotesin.space>
2018-10-20 23:53:28 -0500

    fixed paths

--- pull_bulma.sh
+++ pull_bulma.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 git config core.sparseCheckout true
-echo "css/*" > .git/submodules/theme/source/scss/bulma/info/sparse-checkout
-echo "sass/*" >> .git/submodules/theme/source/scss/bulma/info/sparse-checkout
-echo "bulma.sass" >> .git/submodules/theme/source/scss/bulma/info/sparse-checkout
-echo "CHANGELOG.md" >> .git/submodules/theme/source/scss/bulma/info/sparse-checkout
-echo "LICENSE" >> .git/submodules/theme/source/scss/bulma/info/sparse-checkout
-echo "package.json" >> .git/submodules/theme/source/scss/bulma/info/sparse-checkout
-echo "README.md" >> .git/submodules/theme/source/scss/bulma/info/sparse-checkout
+md -p .git/modules/theme/source/scss/bulma/info
+echo "css/*" > .git/modules/theme/source/scss/bulma/info/sparse-checkout
+echo "sass/*" >> .git/modules/theme/source/scss/bulma/info/sparse-checkout
+echo "bulma.sass" >> .git/modules/theme/source/scss/bulma/info/sparse-checkout
+echo "CHANGELOG.md" >> .git/modules/theme/source/scss/bulma/info/sparse-checkout
+echo "LICENSE" >> .git/modules/theme/source/scss/bulma/info/sparse-checkout
+echo "package.json" >> .git/modules/theme/source/scss/bulma/info/sparse-checkout
+echo "README.md" >> .git/modules/theme/source/scss/bulma/info/sparse-checkout
 cd theme/source/scss/bulma
 # change next tag to current version
 git checkout 0.7.2
